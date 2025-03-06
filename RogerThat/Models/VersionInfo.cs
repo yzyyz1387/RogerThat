0c6d31a4949ecc4c05ab5efe9630cab454fde81e
yzyyz1387 <1796031384@qq.com>
2025-03-07 00:21:40 +0800

    test sync to gitee

--- RogerThat/Models/VersionInfo.cs
+++ RogerThat/Models/VersionInfo.cs
@@ -2,7 +2,7 @@ namespace RogerThat.Models
 {
     public static class VersionInfo
     {
-        public const string Version = "1.0.0-beta10";
+        public const string Version = "1.0.0-beta11";
         public const string ReleaseDate = "2025-03-06";
         
         public static string GetVersionString()
