98bad67950156c175e662ffa0a079167edff39f2
yzyyz1387 <1796031384@qq.com>
2025-03-07 04:48:01 +0800

    v1.0.1-beta1

--- RogerThat/Models/VersionInfo.cs
+++ RogerThat/Models/VersionInfo.cs
@@ -2,7 +2,7 @@ namespace RogerThat.Models
 {
     public static class VersionInfo
     {
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1-beta1";
         public const string ReleaseDate = "2025-03-07";
         
         public static string GetVersionString()
