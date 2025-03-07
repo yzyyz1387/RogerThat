508c41aec5fa67d24eee9e8c70dcc4b7c8dc0226
yzyyz1387 <1796031384@qq.com>
2025-03-07 09:20:12 +0800

    增加统计与隐私

--- RogerThat/Models/VersionInfo.cs
+++ RogerThat/Models/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace RogerThat.Models
     public static class VersionInfo
     {
         public const string Version = "1.0.1-beta2";
-        public const string ReleaseDate = "2025-02-28";
+        public const string ReleaseDate = "2025-03-07";
         
         public static string GetVersionString()
         {
